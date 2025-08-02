b123642af8bab0511e55fb377ef3936b06202860
Prince Kumar <patelprince87145@gmail.com>
2025-08-02 17:34:31 +0000

    Code challenge submission for question: 189 Rotate Array

---
+++ 189 Rotate Array/189rotate-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+         int n = nums.length;
+        k = k % n; 
+
+       
+        reverse(nums, 0, n -k- 1);
+
+   
+        reverse(nums, n-k, n - 1);
+        reverse(nums, 0, n - 1);
+
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}

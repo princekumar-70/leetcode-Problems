7cc1943d5a09dbfdc65dcadd6025f37259dd7f48
Prince Kumar <patelprince87145@gmail.com>
2025-09-21 15:05:50 +0000

    Code challenge submission for question: 33 Search in Rotated Sorted Array

---
+++ 33 Search in Rotated Sorted Array/33search-in-rotated-sorted-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+       int start =0,end = nums.length-1;
+		int ans = -1;
+		while(start<=end) {
+			int mid = start+(end-start)/2;
+			
+			if(nums[mid]==target) {
+				return mid;
+			}else if(nums[mid] <= nums[end]) {
+				if(target > nums[mid] && target<=nums[end]) {
+					start = mid+1;
+				}else {
+					end = mid-1;
+				}
+					
+			}else {
+				if(target >= nums[start] && target < nums[mid]) {
+					end = mid-1;
+				}else {
+					start = mid+1;
+				}
+			}
+		}
+		return -1;
+    }
+}

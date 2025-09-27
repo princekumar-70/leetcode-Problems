941ed1051e9ee7745c1b9963e60e0328c365dd2e
Prince Kumar <patelprince87145@gmail.com>
2025-09-27 05:48:37 +0000

    Code challenge submission for question: 852 Peak Index in a Mountain Array

---
+++ 852 Peak Index in a Mountain Array/852peak-index-in-a-mountain-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0 , end = arr.length-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = start +(end-start)/2;
+
+            if(arr[mid]<arr[mid+1]){
+                ans = mid+1;
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+}

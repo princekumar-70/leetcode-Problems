7ee45aadfbe5ca7c3726eb14017fa1cdc7e074e5
Prince Kumar <patelprince87145@gmail.com>
2025-12-14 10:53:38 +0000

    Code challenge submission for question: 724 Find Pivot Index

---
+++ 724 Find Pivot Index/724find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        for(int x : nums){
+            totalSum += x;
+        }
+        int leftSum = 0;
+        for(int i = 0;i<nums.length;i++){
+            totalSum-=nums[i];
+            if(leftSum==totalSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}

b223cb07321f38108a342c44e25802b99eb5ff5f
Prince Kumar <patelprince87145@gmail.com>
2025-12-18 06:29:16 +0000

    Code challenge submission for question: 283 Move Zeroes

---
+++ 283 Move Zeroes/283move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int x =0;
+        for(int i = 0;i<arr.length;i++){
+           if(arr[i]!=0){
+             int temp = arr[i];
+             arr[i] = arr[x];
+             arr[x] = temp;
+             x++; 
+           }
+        }
+        
+    }
+}

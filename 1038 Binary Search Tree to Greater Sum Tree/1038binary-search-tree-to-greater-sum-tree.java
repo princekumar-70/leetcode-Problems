84de599a02ed583815765072c5648bca1bcd6638
Prince Kumar <patelprince87145@gmail.com>
2026-02-09 17:42:43 +0000

    Code challenge submission for question: 1038 Binary Search Tree to Greater Sum Tree

---
+++ 1038 Binary Search Tree to Greater Sum Tree/1038binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,20 @@
+class Solution {
+    public TreeNode bstToGst(TreeNode root) {
+        ArrayList<TreeNode> arr = new ArrayList<>();
+        Inorder(root, arr);
+        Collections.reverse(arr);
+        int sum =0;
+        for(int i =0;i<arr.size();i++){
+            int app = arr.get(i).val;
+            sum +=app;
+            arr.get(i).val = sum;
+        }
+        return root;
+    }
+    public void Inorder(TreeNode root , ArrayList<TreeNode> arr){
+        if(root==null) return;
+        Inorder(root.left , arr);
+        arr.add(root);
+        Inorder(root.right , arr);
+    }
+}

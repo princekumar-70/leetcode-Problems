e973e42ccefce18f9d5a6c47a148ace12ca8a03a
Prince Kumar <patelprince87145@gmail.com>
2026-02-14 07:06:35 +0000

    Code challenge submission for question: 215 Kth Largest Element in an Array

---
+++ 215 Kth Largest Element in an Array/215kth-largest-element-in-an-array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+      PriorityQueue<Integer> pq = new PriorityQueue<>();
+      for(int elem: nums){
+        pq.add(elem);
+        if(pq.size()>k) pq.remove();
+      }
+      return pq.peek();
+
+    }	
+   
+}

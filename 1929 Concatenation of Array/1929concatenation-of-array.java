177c572f6b4a927a02cc4b83ba99714b15aaa1ee
Prince Kumar <patelprince87145@gmail.com>
2026-06-18 14:02:59 +0000

    Code challenge submission for question: 1929 Concatenation of Array

---
+++ 1929 Concatenation of Array/1929concatenation-of-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int ans[] = new int[2*nums.length];
+        for(int i =0;i<nums.length;i++){
+            ans[i] = nums[i];
+        }
+       int left = nums.length;
+       for(int i =0;i<nums.length;i++){
+        ans[left++]  = nums[i];
+       }
+       return ans;
+
+
+    }    
+}

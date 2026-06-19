ce8d785595ae1612e5788ac4ed2f983ea8e8aeed
Prince Kumar <patelprince87145@gmail.com>
2026-06-19 07:51:23 +0000

    Code challenge submission for question: 155 Min Stack

---
+++ 155 Min Stack/155min-stack.java
@@ -0,0 +1,47 @@
+class MinStack {
+
+    Stack<Long> st;
+    long min;
+
+    public MinStack() {
+        st = new Stack<>();
+    }
+
+    public void push(int val) {
+
+        if(st.isEmpty()) {
+            st.push((long)val);
+            min = val;
+        }
+        else if(val >= min) {
+            st.push((long)val);
+        }
+        else {
+            st.push(2L * val - min);
+            min = val;
+        }
+    }
+
+    public void pop() {
+
+        long top = st.pop();
+
+        if(top < min) {
+            min = 2 * min - top;
+        }
+    }
+
+    public int top() {
+
+        long top = st.peek();
+
+        if(top < min)
+            return (int)min;
+
+        return (int)top;
+    }
+
+    public int getMin() {
+        return (int)min;
+    }
+}

7848398a84603ae73f0a245bca077f9ec148abf3
Prince Kumar <patelprince87145@gmail.com>
2026-06-20 16:39:51 +0000

    Code challenge submission for question: 122 Best Time to Buy and Sell Stock II

---
+++ 122 Best Time to Buy and Sell Stock II/122best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buy = 0;
+        int sell = 1;
+        while(sell<prices.length){
+            if(prices[sell]>prices[buy]){
+                profit+=(prices[sell]-prices[buy]);
+            }
+            buy = sell;
+            sell++;
+        }
+        return profit;
+    }
+}

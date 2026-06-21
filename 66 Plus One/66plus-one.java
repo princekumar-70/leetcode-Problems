ab1b5de3fc40706adc956c0e5e484d7faed3bbd5
Prince Kumar <patelprince87145@gmail.com>
2026-06-21 05:44:00 +0000

    Code challenge submission for question: 66 Plus One

---
+++ 66 Plus One/66plus-one.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+ 
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[digits.length+1];
+        result[0] = 1;
+
+       
+       
+
+        return result;
+    }
+}
